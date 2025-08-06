bf48b6ccd0553a56c6996f3474ec5d6bffb6684a
gtbnhyujmj <gtbnhyujmj@gmail.com>
2025-08-06 12:26:39 +0800

    Create views.py

---
+++ views.py
@@ -0,0 +1,41 @@
+# accounts/views.py
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# ===
+
+# [register] logic
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # 或導向你想要的首頁
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+# ===
+
+# [log-in] logic
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+# ===
+
+# [log-out] logic
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# ===
